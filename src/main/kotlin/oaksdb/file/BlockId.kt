d75b05f5e9866a070c72ed8fcfc52688be4360c9
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2025-07-14 21:15:52 +0900

    fix syntax

--- src/main/kotlin/oaksdb/file/BlockId.kt
+++ src/main/kotlin/oaksdb/file/BlockId.kt
@@ -2,7 +2,7 @@ package oaksdb.file
 
 data class BlockId(val fileName: String, val blockNum: Int) {
     override fun toString(): String {
-        return "[file ${fileName}, block ${blockNum}]"
+        return "[file $fileName, block $blockNum]"
     }
 
     override fun hashCode(): Int {
