e93b802faf83da32c211fb6177c10a081d13a76e
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2025-08-03 14:11:32 +0900

    FileManagerTest

--- src/test/kotlin/oaksdb/file/FileManagerTest.kt
+++ src/test/kotlin/oaksdb/file/FileManagerTest.kt
@@ -8,20 +8,22 @@ import kotlin.test.assertEquals
 class FileManagerTest {
 
     companion object {
-        private const val TEST_FILE_BASE_DIR = "src/test/resources/oaksdb/file/FileManager"
+        private const val BLOCK_SIZE = 1024
     }
 
     @Nested
     inner class Constructor {
+        private val dirName = "constructor"
+
         @Test
         fun `ディレクトリが存在しない場合、新しいディレクトリが作成されること`() {
             // given
-            val dbDirectory = File(TEST_FILE_BASE_DIR)
+            val dbDirectory = File(dir(dirName))
 
             // when
             FileManager(
                 dbDirectory = dbDirectory,
-                blockSize = 1024
+                blockSize = BLOCK_SIZE
             )
 
             // then
@@ -34,7 +36,7 @@ class FileManagerTest {
         @Test
         fun `tempから始まるファイルが削除されること`() {
             // given
-            val dbDirectory = File(TEST_FILE_BASE_DIR).also {
+            val dbDirectory = File(dir(dirName)).also {
                 it.mkdirs()
             }
             val tempFile1 = File(dbDirectory, "temp1.odb").also {
@@ -50,7 +52,7 @@ class FileManagerTest {
             // when
             FileManager(
                 dbDirectory = dbDirectory,
-                blockSize = 1024
+                blockSize = BLOCK_SIZE
             )
 
             // then
@@ -65,13 +67,15 @@ class FileManagerTest {
 
     @Nested
     inner class IsNew {
+        private val dirName = "isNew"
+
         @Test
         fun `ディレクトリが存在しない場合はtrue`() {
             // given
-            val dbDirectory = File(TEST_FILE_BASE_DIR)
+            val dbDirectory = File(dir(dirName))
             val sut = FileManager(
                 dbDirectory = dbDirectory,
-                blockSize = 1024
+                blockSize = BLOCK_SIZE
             )
             dbDirectory.deleteRecursively()
 
@@ -85,10 +89,10 @@ class FileManagerTest {
         @Test
         fun `ディレクトリが存在する場合はfalse`() {
             // given
-            val dbDirectory = File(TEST_FILE_BASE_DIR)
+            val dbDirectory = File(dir(dirName))
             val sut = FileManager(
                 dbDirectory = dbDirectory,
-                blockSize = 1024
+                blockSize = BLOCK_SIZE
             )
 
             // when
@@ -103,14 +107,101 @@ class FileManagerTest {
     }
 
     @Nested
-    inner class Read {}
+    inner class ReadWrite {
+        val dirName = "read-write"
+
+        @Test
+        fun `ファイルの読み書きができる`() {
+            // given
+            val expected = "Hello, World!"
+            val block = BlockId("data.odb", 0)
+            val writePage = Page(ByteArray(BLOCK_SIZE)).apply {
+                setString(0, expected)
+            }
+            val readPage = Page(ByteArray(BLOCK_SIZE))
+            val dbDirectory = File(dir(dirName))
+            val sut = FileManager(
+                dbDirectory = dbDirectory,
+                blockSize = BLOCK_SIZE
+            )
+
+            // when
+            sut.write(block, writePage)
+            sut.read(block, readPage)
+
+            // then
+            assertEquals(expected, readPage.getString(0))
+
+            // cleanup
+            dbDirectory.deleteRecursively()
+        }
+    }
 
     @Nested
-    inner class Write {}
+    inner class Append {
+        private val dirName = "append"
+
+        @Test
+        fun `ファイルに新しいブロックを追加できる`() {
+            // given
+            val fileName = "data.odb"
+            val block = BlockId(fileName, 0)
+            val page = Page(ByteArray(BLOCK_SIZE)).apply {
+                setString(0, "Hello, World!")
+            }
+            val dbDirectory = File(dir(dirName))
+            val sut = FileManager(
+                dbDirectory = dbDirectory,
+                blockSize = BLOCK_SIZE
+            )
+            sut.write(block, page)
+            val expected = BlockId(fileName, block.number + 1)
+
+            // when
+            val actual = sut.append(block.fileName)
+
+            // then
+            assertEquals(expected, actual)
+
+            // cleanup
+            dbDirectory.deleteRecursively()
+        }
+    }
 
     @Nested
-    inner class Append {}
+    inner class Length {
+        private val dirName = "length"
 
-    @Nested
-    inner class Length {}
+        @Test
+        fun `ファイルの長さを取得できる`() {
+            // given
+            val expected = 5
+            val fileName = "data.odb"
+            val dbDirectory = File(dir(dirName))
+            val sut = FileManager(
+                dbDirectory = dbDirectory,
+                blockSize = BLOCK_SIZE
+            )
+            repeat(expected) {
+                val block = BlockId(fileName, it)
+                val page = Page(ByteArray(BLOCK_SIZE)).apply {
+                    setString(0, "Hello, World!")
+                }
+                sut.write(block, page)
+            }
+
+            // when
+            val actual = sut.length(fileName)
+
+            // then
+            assertEquals(expected, actual)
+
+            // cleanup
+            dbDirectory.deleteRecursively()
+        }
+    }
+
+    private fun dir(dirName: String): String {
+        return "src/test/resources/oaksdb/file/FileManager/$dirName"
+    }
 }
